1f807bf3b8dc5007a1ffcd4d0a60bdbb726357c6
Srikanth B <srikanthbanchode89@gmail.com>
2026-07-24 18:59:52 +0530

    Add GitHub Actions CI workflow and ESLint configuration

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+      "eqeqeq": "error"
+    }
+  }
+];
